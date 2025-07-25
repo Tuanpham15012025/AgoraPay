723b4a75380a9a4a30e61a3bc7500d29a45608ef
TuanPham237 <phamtuandhxdk40@gmail.com>
2025-07-25 23:01:39 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PiLogin from './components/PiLogin';
+
+function App() {
+  return (
+    <div className="App">
+      <PiLogin />
+    </div>
+  );
+}
+
+export default App;
