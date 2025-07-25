e7f182b1f4fb8e239bc250f245e0cea43f1baea5
TuanPham237 <phamtuandhxdk40@gmail.com>
2025-07-25 15:59:52 +0700

    Create App.tsx
    Create App.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// File: /agorapay/src/App.tsx
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
