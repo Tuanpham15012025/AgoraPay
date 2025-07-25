0161c797c17aaf58fdf0d0318f7b63c2e1668025
TuanPham237 <phamtuandhxdk40@gmail.com>
2025-07-25 21:47:51 +0700

    Create Header.tsx
    Create Header.tsx file

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Header = () => (
+  <header className="w-full p-4 bg-purple-600 text-white text-xl font-bold shadow-md">
+    AgoraPay Wallet
+  </header>
+);
