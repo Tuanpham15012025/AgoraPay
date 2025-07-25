b9957d09221fd5ceeae10ed1ae65ab319974779f
TuanPham237 <phamtuandhxdk40@gmail.com>
2025-07-25 23:02:58 +0700

    Update App.js
    Update App.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import PiLogin from './components/PiLogin';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>AgoraPay</h1>
+      <p>Peer-to-Peer Pi Payments</p>
       <PiLogin />
     </div>
   );
