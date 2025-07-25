26b09aee7541bca11465868d3374bfda483e56ab
TuanPham237 <phamtuandhxdk40@gmail.com>
2025-07-25 21:40:17 +0700

    Create Wallet.tsx
    Create Wallet.tsx

---
+++ src/pages/Wallet.tsx
@@ -0,0 +1,13 @@
+```tsx
+import React from 'react';
+
+export default function Wallet() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold">My Wallet</h2>
+      <p>Balance: 10,000 Pi</p>
+      <p>Bridged Assets: 100 XRP, 150 XLM</p>
+    </div>
+  );
+}
+```
